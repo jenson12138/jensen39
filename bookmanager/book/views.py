95e44cc58dc0fb73038c508db426c4dfd926b844
Jensen <jenson12138@gmail.com>
2025-05-08 18:22:46 -0700

    定义视图函数

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+'''
+python function is a view.
+1. first parameter is a request, which is a class object for httprequest
+'''
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+
+# we are looking for users to use
+def index(request):
+    pass
+
+# def index(request)
+#
+#     return HttpResponse('ok')
+
